ae38fbb6c67cfc4c587b0f42f9ce516762315049
eliesatpanelgrid <eliesatpanelgrid@gmail.com>
2026-06-26 12:23:22 +0300

    Add files via upload

--- menus/Iptvadder/Iptvadder.py
+++ menus/Iptvadder/Iptvadder.py
@@ -1,2 +1,391 @@
-import zlib,base64
-exec(zlib.decompress(base64.b64decode('eNrtWu9y28YR/86nuMLjBKgpkPrjVOGY8ciy4mgqSxpJzkxGYjAQcCRhgQDmcBCldvIW/d6n6Hv1Ebr3DzjcgRTlJE0/VDOJycPu3t7u3u5vF3yBtv68haI8TrLZCFV0urXPVnrJosgJRXmpPhHcm5J8gXCWzBYhkqv4Kllg0kczTN/j8o7mRU+QnafVLMlK/+iB4qxMcvYxTfBlSM/DDKcfSBL7C5xVpf8DTgtMSiXR7SH4A3lBmkdhGiRFn69EcxzdBUlGMckw7ddUySKc4SALF7hZKx7pPM+CexALOzfrJc0Jo06yad6sknAhVzyp/GVEMGjti3+VZuJbm+IjLkuQ+C5/UFTNiqA8zGE9wxkt/YOIgjofw0LR1gsW6Ul4i1NFxr9YJFGeTZOZojnk367wA+3Lz5c4xVw+945YO0lKepRR8mhJa563JbIV/eCb+DUAcyY0CJSkH4UfnhsYx+CT28awxwW9l5r0elEaliVfCuMYE1c86FtKe6Me9/Myiem8j+Y4mc0pGmvx6g49v0z+hl3P50Su1+9+KnhdTwgs75IsmCYpBmEiZNmfM6hKMkiT24G4JjsDEYoDeepBc+qBeeoBP/WgPtMgUZ+C6Tz2HxapU++TTMWJ0HdjtP3tzrB+gNMS/15a6EpIK0Akjeq9lwkolBc4c2vj9JFDHA+FJZo2dMp8YLmpT3AYg0257g8RLmhDJ2mcNyV35XdvBvKDIzaP8RSpUHNLnE77qITLB8fyGiEiDPwVdM1esOzLVdhTfjIf06uEptgNXOeyugVlwDaM/ihOILE4njRKzUAwSzg4WOQxC5Nh++ltWmmP6mcv0NXxx6ML5L4GsgiMmsX50utgpSzzAq9Iwe5KEh+yaHobRnd+WIBzYm4CP6gJ8oqaiqc8+4zNROK2XAjmD6uUjp2ogmMunH7raTTPkwiX4+vWKvtzawakPnn9DqoSE0jgD9uMrv68jnJHo9xZS7mrUe6updzTKPe6KT/jguSalkgudBKDwUio0Yrv3RpQkmez/TtdtFhDbHEtz04XzwqbZDjfa20iF1YT75jE6yTvmsS7a4j3TGLL5pPmqxm1FWlilhVCt47QOaXFaDAAAqiaEG5tPl5duhn3hyZxBdoxsLGCgT225EOpWuYkXsHCHjudN9CHvHua02SawP1uloNoHmYzHPcRy2kJYCR2w8ffh5D7TTlRyir6GLWvoY0HXIeDjJHT1/Y3TN/F9enipOYB827AcQ7WrlmY6TfZRRq92UoubLKftH6zp1zQeCeN1UwAYVcOZdW6iow7q8m1IwCaM/FTVjqYSLfhNhx17aR4SoPbkDgTcBd3huvcZI7/OU8yl3G4jsRRyEGvFKbyPHNXwlDKE4LePSJZ9x3PszRpELUu4pitjvjebdjtWioo4N5iP2941XOb81IAdIb8NGYTutt8FwcfO3gUsLfpmwZB1/GcrzZ6ttsIW0rG9QppVepSTjEVItodi2sbmuBYYwRUccg40HlI54612YxFY5v8cp4v0QBdCJgB3USUV4Dlbd5HnKb50mAGKIBC+O8dQIOqsJkYQDDUS3FIAMCHjyyMSsc+UcFgZFCwA2gWcSzz1yIaKn41uNXVwyBOSGmZzRd7wDOO02BhChApqFdNIJTmYRyU4T2G/2ugzbV0DznGESrVjVkb8lw7Z3eHYRbh9EASQ7k6zNOcqO+TdkL6u1XouNNHElCC/4JE14qbzq6O0vmjNqxk0Bl6b5taulvtAo4ObrmTO2i5l0cqrYF/G/t3UEf88A19XuI21S/tr1vbVrl+gbaMPwl4zeUG4+tQlceJhu87oPTqkOQQnhVfRxVcJr8j6IxNCtE3sTo6qRenOUEkz6EOBH3Emp0S6jHKS38ZpneuM8A0Uv2X47W7H2jfmjvg0wfqMFYuY2QZfZqnMQf6IJodw78NwaUs8bLdPYteohPBBsECG0XYdQIWq8jxmpUttQIIkSVjn/LuxpYI6nKhLFvwD1lOmcLSLLbKms1U08GrdXPlCKYVgUp2cB8mgDWgjVY2ru0yuslclkaZlqJ4SRKPLXoOU0sxibb3CWFvmNpTSJHxd57Tdj/gJy0EuCPMGBAeXhMBv9r9Xb5nmyoTKv9zW4h9DTFaDpcG5gKas0LsE8hzAQuhAPCaBDVpkmFNxwUck2B/EdJo7pKvXQafr8vJW4DQ1z8PJq+8ryWLudvCn5G8Ktxtj51xIdxymmd45dV/d/LpCL37dHV1doo+nb8/uDp6vzoRGPnJ9BALU/Bwu0i0bSr01DJ/dStnOC3jtkWAmVtJWpha79j//c9//At9f3xxeYUOT44P/6qrZCYoyFCjXkupF+Jg6Oz05KcmYpuUoodSr50ZCJoyrRlVXT+7YtmzQ6s1utH/+ASHi4WDLyE98DTfkRbMeU3r9gPIbp9yTTa260yNMYRpcOzfZOdQ90oRMOEshBOzeRP8w+ckeRaXiObSkrq3nJZ0z1aqVT22VzwWkxQAe4S6r4fDYR9dkQp7a4NLxsXl0eHZ6XsRGMjVlW7YLWfYO+eF662JG/3IPHTMjKMCHD+IuyMivyMwxARPPDYiYFOHtsdj/IZJP+r3pit+1gk9IgQCngtKslnbySZI7BiudWSfiyO4sWffo5/OPl2gs4vjD8enByfo8Oz9EXI/nR7+cHD64ei9ZyekOiOtApd/bGYytt3I8+b+VkRq0906Nrpnu+yPlYdSG/D6ZZECumCrZhyBlilmnSk889AbtGNLM5UzxiQQIT+G4HAh43o4UYimfT8ZwL9nFhVkW9s1XVtyq/zZuhj1cMQKovt2NHJv4lee93YAOfjGL+bFzVs1pRi71z9/BVRfqdGDWvjahthCyf66pMVKq20jNYJprNFUY6+bmk1fbPIdD8E105u2zgGYzbi7ch95bJtlz+tKyARPIdfPAzFA0SpPV8ZoKg2/kPqATKIbISbAKV7gjOo4B4AVyyRjo09TY+kRcs3RIQQ9H/Ex6MXndm1PNYNqjTcKpzmlkp2hIfH/Faw7Out9UjzQez/DdBPWXZ2VrT34Bck34dzTOecPt/uf/ZzC5uzxk4rLebcmIdzdex37bPSyllEMv3UrswV/lkOx8xd4veL1dFzxlyBgCjf7m+2/fLPnL2P/tirZRS+3oHxV8db+nU+qjWTutGRG0OjG2da3w6dUknNz3Q4FSdL94XD/2w59gPwpheRwXY+l6ZeKaQUHOOxL5bRCBWLzWWJ+aW77HX6Ei9e+m/49yw56/WJUAJfkTW3ffLiTfcRuYx8VSzZdl1TXwDTprc+NbFZtJlWu0bi5/ZvmP7ayceYDTe3XhEa6q7OZ8aRr9PH/Af9mA/5njeZXEAPpYUUIBOpxFuMHd6i5yhhCWq4KSTQPWNPOZwjOYIHjJBzM45iVEvltsYicSW9dU6BJeWX6nGN3HStKsbBB7HVMlQiK5cSCHZoprRg6J0Cx6H5KhgNdKIK3BtlkwyqtD05E39ocyuQBxGngXzn20H+1IGqxNLwfTWeO90Tvwz57GwzaQB6fs3F6+4J3I3Qh5Qlk/iUIfYP+rBs3y4OZw5pmLsPid8UsujPnyPddPtek+VlOK3HpI+r/lUHNc6y9rhE9zUVPy2eJjvff6KOuU9Wr8MuT8oEh761kT8XbKEUzeXaH9cRxQ/lqCfFe1/E26tBqJtVxbY9sxTp+KMMnZM9QsPOWOJdMU/VObHST3WR8mFy/CVXa2WdZUcuNX/Nsb2SD32dE18SQmtOtjqAmK8Nmott5bjA9Z/Cnb2N4m7dcmEBjxiMCam6cRNRnvwoE6FW6NeurOnTM9rD78Mu1h5/6S5JQrL0EF0p4MhyeMdZSb1hVfovZyLF5men1ngqa4a+dfjEnMROxmNHfYWkv+X6DbKvuJR8QcEyugqHNl+FlwEKG/UrvZTl6WbLhB5t9NKOPl2Uz9YDP9LHA48VuBeFflV/lFS0qOgbToZfIXT3S4Cr01wwx1hHUgH2tFAXUO4i81q8r11ngDzj+b3C6nn4Y9mZIvVi33hap03q/cWGuq9yk99yJdWeOfF6NfXpeuUnyayNcMYeSkaGNKkWDq79UpJjwhGi8V+RJuS6b7e3W+sjuazn5n8a1eztfM3I11JvGtMs3zZEmkDlrzV+haxUWWr7v8kB3mrbTs8QpVnb+4lr6RIx8aRmpveN9SYSsy/XyTSiglpc8NzT37j+9LwMc')))
+# -*- coding: utf-8 -*-
+import os
+import re
+from enigma import eTimer, getDesktop
+
+from Plugins.Extensions.ElieSatPanelGrid.menus.Helpers import (
+    get_local_ip,
+    check_internet,
+    get_image_name,
+    get_python_version,
+    get_storage_info,
+    get_ram_info,
+)
+
+from Screens.Screen import Screen
+from Screens.MessageBox import MessageBox
+from Components.ActionMap import ActionMap
+from Components.Label import Label
+from Components.config import ConfigText, ConfigSelection, getConfigListEntry
+from Components.ConfigList import ConfigListScreen
+from Plugins.Extensions.ElieSatPanelGrid.__init__ import Version
+from Plugins.Extensions.ElieSatPanelGrid.menus.Infobox import IptvScreen
+
+
+class Iptvadder(Screen, ConfigListScreen):
+
+    width, height = getDesktop(0).size().width(), getDesktop(0).size().height()
+
+    skin_file = (
+        "/usr/lib/enigma2/python/Plugins/Extensions/ElieSatPanelGrid/menus/Iptvadder/iptvadder_fhd.xml"
+        if width >= 1920
+        else "/usr/lib/enigma2/python/Plugins/Extensions/ElieSatPanelGrid/menus/Iptvadder/iptvadder_hd.xml"
+    )
+
+    try:
+        with open(skin_file, "r") as f:
+            skin = f.read()
+    except:
+        skin = "<screen></screen>"
+
+    def __init__(self, session):
+        Screen.__init__(self, session)
+        self.session = session
+        self.setTitle(_("Subscription Editor"))
+
+        self.restore_mode = 0
+        self.blue_mode = 0
+
+        # TIMER (5 sec window)
+        self.blue_timer = eTimer()
+        self.blue_timer.callback.append(self._blue_timeout)
+
+        self.label = ConfigSelection(
+            default="custom",
+            choices=[
+                ("custom", "custom"),
+                ("serverx1", "serverx1"),
+                ("serverx2", "serverx2"),
+                ("serverx3", "serverx3"),
+                ("serverx4", "serverx4"),
+                ("serverx5", "serverx5"),
+                ("serverx6", "serverx6"),
+                ("serverx7", "serverx7"),
+                ("serverx8", "serverx8"),
+                ("jepro1", "server jepro1"),
+                ("ultra", "server ultra"),
+                ("strong8k1", "server strong 8k1"),
+                ("strong8k2", "server strong 8k2"),
+                ("neo4k1", "server neo4k1"),
+                ("neo4k2", "server neo4k2"),
+                ("neo4k3", "server neo4k3"),
+                ("neo4k4", "server neo4k4"),
+            ],
+        )
+
+        self.url = ConfigText(default="http://url.com")
+        self.port = ConfigText(default="80")
+        self.username = ConfigText(default="user")
+        self.password = ConfigText(default="pass")
+
+        self.label.addNotifier(self.label_changed, initial_call=False)
+
+        self.clist = [
+            getConfigListEntry("Label:", self.label),
+            getConfigListEntry("URL:", self.url),
+            getConfigListEntry("Port:", self.port),
+            getConfigListEntry("Username:", self.username),
+            getConfigListEntry("Password:", self.password),
+        ]
+
+        ConfigListScreen.__init__(self, self.clist, session=session)
+        self["config"].l.setList(self.clist)
+
+        self["left_bar"] = Label("\n".join(list("Version " + Version)))
+        self["right_bar"] = Label("\n".join(list("By ElieSat")))
+
+        self["image_name"] = Label("Image: " + get_image_name())
+        self["local_ip"] = Label("IP: " + get_local_ip())
+        self["StorageInfo"] = Label(get_storage_info())
+        self["RAMInfo"] = Label(get_ram_info())
+        self["python_ver"] = Label("Python: " + get_python_version())
+        self["net_status"] = Label("Net: " + check_internet())
+
+        self["red"] = Label(_("Check Path"))
+        self["green"] = Label(_("Show / Restore Accounts"))
+        self["yellow"] = Label(_("Send and Backup"))
+        self["blue"] = Label(_("Clear Playlists"))
+
+        self["panel_path"] = Label("")
+        self["playlists"] = Label(self.get_playlists_dirs())
+
+        self.panel_dir = self.find_panel_dir()
+        self.load_saved_subscription()
+
+        self["actions"] = ActionMap(
+            ["OkCancelActions", "ColorActions"],
+            {
+                "red": self.show_isubscription_path,
+                "green": self.restore_reader,
+                "yellow": self.send_backup,
+                "blue": self.clear_playlists,
+                "cancel": self.close,
+            },
+            -1,
+        )
+
+    # ---------------- TIMER ----------------
+    def _blue_timeout(self):
+        self.blue_mode = 0
+        self["panel_path"].setText("")
+
+    def get_playlists_dirs(self):
+        plugins = []
+        for root, _, files in os.walk("/etc/enigma2"):
+            if "playlists.txt" in files:
+                folder = os.path.basename(root)
+                name = folder.replace("_", " ").replace("-", " ").strip().title()
+                if name and name not in plugins:
+                    plugins.append(name)
+
+        return "Available plugins playlists:\n(" + ", ".join(plugins) + ")" if plugins else "Available plugins playlists:\n(<not found>)"
+
+    def get_all_playlists_files(self):
+        files = []
+        for root, _, fs in os.walk("/etc/enigma2"):
+            if "playlists.txt" in fs:
+                files.append(os.path.join(root, "playlists.txt"))
+        return files
+
+    def extract_base_url(self, line):
+        m = re.match(r'(http[s]?://[^/]+)', line)
+        return m.group(1) if m else None
+
+    # ---------------- BLUE BUTTON UPDATED ----------------
+    def clear_playlists(self):
+        if not self.panel_dir:
+            return
+
+        subfile = os.path.join(self.panel_dir, "isubscription.txt")
+
+        # 🔵 FIRST CLICK
+        if self.blue_mode == 0:
+
+            # clear ONLY playlists files in /etc/enigma2
+            for fpath in self.get_all_playlists_files():
+                try:
+                    open(fpath, "w").close()
+                except:
+                    pass
+
+            self["panel_path"].setText(
+                "Playlists cleared.\nPress BLUE again within 5 seconds to clear subscription"
+            )
+
+            self.blue_mode = 1
+            self.blue_timer.start(5000, True)
+            return
+
+        # 🔵 SECOND CLICK (within 5 sec)
+        try:
+            self.blue_timer.stop()
+
+            # clear ONLY subscription file
+            if os.path.exists(subfile):
+                open(subfile, "w").close()
+
+            self["panel_path"].setText("Subscription file cleared")
+
+        except:
+            self["panel_path"].setText("Error clearing subscription")
+
+        self.blue_mode = 0
+
+    # ---------------- REST OF YOUR ORIGINAL CODE (UNCHANGED) ----------------
+
+    def load_saved_subscription(self):
+        if not self.panel_dir:
+            return
+
+        subfile = os.path.join(self.panel_dir, "isubscription.txt")
+        if not os.path.exists(subfile):
+            return
+
+        try:
+            with open(subfile, "r") as f:
+                lines = f.read().splitlines()
+
+            if len(lines) < 2:
+                return
+
+            self.label.setValue(lines[0].strip())
+            active = lines[-1].strip()
+
+            m = re.match(
+                r'(http[s]?://[^:/]+)(?::(\d+))?/get\.php\?username=([^&]+)&password=([^&]+)',
+                active,
+            )
+
+            if m:
+                self.url.setValue(m.group(1))
+                self.port.setValue(m.group(2) or "")
+                self.username.setValue(m.group(3))
+                self.password.setValue(m.group(4))
+
+            self.refresh_config()
+        except:
+            pass
+
+    def label_changed(self, config_element):
+        mapping = {
+            "custom": ("http://url.com", "user", "pass"),
+            "serverx1": ("http://cafott.com", None, None),
+            "serverx2": ("http://vipxtv.net", None, None),
+            "serverx3": ("http://servx.pro", None, None),
+            "serverx4": ("http://hxb8j.otvipserv.com", None, None),
+            "serverx5": ("http://smartott.org", None, None),
+            "serverx6": ("http://vireexaa.com", None, None),
+            "serverx7": ("http://rfcot.com", None, None),
+            "serverx8": ("http://qwerlo.com", None, None),
+            "jepro1": ("http://a345d.info", None, None),
+            "ultra": ("http://ultra.gotop.me", None, None),
+            "strong8k1": ("https://fine61764.wd.business-cloud-8k.ru", None, None),
+            "strong8k2": ("https://cf.cdn-90.me", None, None),
+            "neo4k1": ("http://april80089.wd.business-cloud-neo.ru", None, None),
+            "neo4k2": ("http://cf.business-cloud-neo.ru", None, None),
+            "neo4k3": ("http://pro.business-cloud-neo.ru", None, None),
+            "neo4k4": ("http://tv.business-cloud-neo.ru", None, None),
+        }
+
+        key = config_element.value
+        if key in mapping:
+            url, user, pwd = mapping[key]
+            self.url.setValue(url)
+            if key == "custom":
+                self.username.setValue(user)
+                self.password.setValue(pwd)
+
+        self.refresh_config()
+
+    def refresh_config(self):
+        self.clist = [
+            getConfigListEntry("Label:", self.label),
+            getConfigListEntry("URL:", self.url),
+            getConfigListEntry("Port:", self.port),
+            getConfigListEntry("Username:", self.username),
+            getConfigListEntry("Password:", self.password),
+        ]
+        self["config"].l.setList(self.clist)
+        self["config"].setCurrentIndex(0)
+
+    def find_panel_dir(self):
+        search_roots = ["/media/hdd", "/media/mmc"]
+
+        try:
+            search_roots += [
+                os.path.join("/media", d)
+                for d in os.listdir("/media")
+                if d.startswith("usb")
+            ]
+        except:
+            pass
+
+        for root in search_roots:
+            path = os.path.join(root, "ElieSatPanel", "panel_dir.cfg")
+            if os.path.exists(path):
+                folder = os.path.dirname(path)
+                subfile = os.path.join(folder, "isubscription.txt")
+                if not os.path.exists(subfile):
+                    open(subfile, "w").close()
+                return folder
+        return None
+
+    def show_isubscription_path(self):
+        self.session.open(IptvScreen)
+
+    def restore_reader(self):
+        if not self.panel_dir:
+            return
+
+        subfile = os.path.join(self.panel_dir, "isubscription.txt")
+
+        if not os.path.exists(subfile):
+            self["panel_path"].setText("No file found")
+            return
+
+        try:
+            with open(subfile, "r") as f:
+                lines = [l.strip() for l in f.readlines() if l.strip()]
+
+            if len(lines) < 2:
+                self["panel_path"].setText("No accounts saved")
+                return
+
+            accounts = lines[1:]
+
+            if self.restore_mode == 0:
+                self["panel_path"].setText(
+                    "Saved Account:\n\n" + "\n".join(accounts)
+                )
+                self.restore_mode = 1
+                return
+
+            for fpath in self.get_all_playlists_files():
+                try:
+                    with open(fpath, "r") as f:
+                        existing = [l.strip() for l in f.readlines() if l.strip()]
+                except:
+                    existing = []
+
+                merged = list(dict.fromkeys(existing + accounts))
+
+                with open(fpath, "w") as f:
+                    f.write("\n".join(merged) + "\n")
+
+            self["panel_path"].setText("Accounts restored to playlists")
+            self.restore_mode = 0
+
+        except:
+            self["panel_path"].setText("Error reading file")
+
+    def send_backup(self):
+        if not self.panel_dir:
+            return
+
+        if self.port.value.strip():
+            new_line = "%s:%s/get.php?username=%s&password=%s&type=m3u_plus&output=ts" % (
+                self.url.value,
+                self.port.value,
+                self.username.value,
+                self.password.value,
+                )
+        else:
+            new_line = "%s/get.php?username=%s&password=%s&type=m3u_plus&output=ts" % (
+                self.url.value,
+                self.username.value,
+                self.password.value,
+            )
+
+        new_base = self.extract_base_url(new_line)
+
+        subfile = os.path.join(self.panel_dir, "isubscription.txt")
+
+        lines = []
+        if os.path.exists(subfile):
+            try:
+                with open(subfile, "r") as f:
+                    lines = f.read().splitlines()
+            except:
+                pass
+
+        label_line = self.label.value
+
+        filtered = []
+        for l in lines[1:]:
+            base = self.extract_base_url(l)
+            if base != new_base:
+                filtered.append(l)
+
+        lines = [label_line] + filtered + [new_line]
+
+        with open(subfile, "w") as f:
+            f.write("\n".join(lines) + "\n")
+
+        for fpath in self.get_all_playlists_files():
+            try:
+                with open(fpath, "w") as f:
+                    f.write("\n".join(lines[1:]))
+            except:
+                pass
+
+        self["panel_path"].setText("UPDATED:\n%s" % new_line)
